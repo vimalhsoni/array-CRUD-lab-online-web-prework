79d7a61b48b3cd9dd916570bee4b86040b820357
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-04 20:44:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-create_an_arry.unshift("Luc")
+create_an_array.unshift("Luc")
 end
 
 def add_element_to_start_of_array(array, element)
