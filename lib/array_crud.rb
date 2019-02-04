d1fcd6261b705abdbdfc043b777a941e517ae89d
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-04 20:40:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = [michael, scottie, rodman]
 end
 
 def add_element_to_end_of_array(array, element)
